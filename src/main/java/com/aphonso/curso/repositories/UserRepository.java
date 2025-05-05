4d4b42d6da5fc45721de1139d96075986e8868d1
pHMiguel <aphonso99@gmail.com>
2025-05-05 01:47:15 -0300

    Dependency Injection, DB Seeding

--- src/main/java/com/aphonso/curso/repositories/UserRepository.java
+++ src/main/java/com/aphonso/curso/repositories/UserRepository.java
@@ -1,9 +1,9 @@
-package com.aphonso.curso.repository;
+package com.aphonso.curso.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.aphonso.curso.entities.User;
 
 public interface UserRepository extends JpaRepository<User, Long>{
-
+    
 }
