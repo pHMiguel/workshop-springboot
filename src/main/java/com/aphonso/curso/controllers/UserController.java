e34fa1ddc9c4816e91067406754d9b466e701fa0
Aphonso Miguel <aphonso99@gmail.com>
2025-05-14 15:37:34 -0300

    User insert

--- src/main/java/com/aphonso/curso/controllers/UserController.java
+++ src/main/java/com/aphonso/curso/controllers/UserController.java
@@ -1,13 +1,18 @@
 package com.aphonso.curso.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.aphonso.curso.entities.User;
 import com.aphonso.curso.services.UserService;
@@ -32,6 +37,10 @@ public class UserController {
         return ResponseEntity.ok().body(obj);
     }
     
-    
-    
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User user){
+    	user = service.insert(user);
+    	URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
+    	return ResponseEntity.created(uri).body(user);
+    }
 }
